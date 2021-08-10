1ee3555a38a0098a62e3c2c214cda8df0746727b
charlô <charlesoliveira0293@hotmail.com>
2021-08-10 00:05:04 -0300

    Criação da classe Estoque

--- Projeto-Java/src/classes/Estoque.java
+++ Projeto-Java/src/classes/Estoque.java
@@ -1,6 +1,51 @@
 package classes;
 
 public class Estoque {
+	
+	private int quantidade;
+	private String codProduto;
+	private boolean emprestimo;
+	private int tipoDef;
+	private String indeterminado;
+	
+	public Estoque(int quant, String codigo, boolean emprestimo, int tipoDef) 
+	{
+		this.quantidade = quant;
+		this.codProduto = codigo;
+		this.emprestimo = emprestimo;
+		this.tipoDef = tipoDef;
+	}
 
-	private String nome;
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public int getTipoDef() {
+		return tipoDef;
+	}
+
+	public void setTipoDef(int tipoDef) {
+		this.tipoDef = tipoDef;
+	}
+
+	public String getCodProduto() {
+		return codProduto;
+	}
+
+	public void setCodProduto(String codProduto) {
+		this.codProduto = codProduto;
+	}
+
+	public boolean isEmprestimo() {
+		return emprestimo;
+	}
+
+	public void setEmprestimo(boolean emprestimo) {
+		this.emprestimo = emprestimo;
+	}
+	
 }
