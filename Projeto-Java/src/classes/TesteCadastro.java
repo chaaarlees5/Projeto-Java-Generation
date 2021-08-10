72f2850e205b621d91be05cc4a3db2d6c1404397
Hopkins-x <silvalucs212@gmail.com>
2021-08-10 11:55:41 -0300

    Criação teste cadastro

---
+++ Projeto-Java/src/classes/TesteCadastro.java
@@ -0,0 +1,31 @@
+package classes;
+
+public class TesteCadastro {
+
+	public static void main(String[] args) 
+	{
+		int qual;
+		Cadastro cadastro = new Cadastro (null, null, null, null, null, null);
+		System.out.println("Digite se você é Pessoa Física ou Pessoa Jurídica: ");
+			
+		//switch(qual);
+		//case 1;
+		
+		/*for(int x = 0; x < quant; x++) {
+			c1 = new Cadastro(null, null, null, null, null);
+			c1.getClass();
+			quant++;
+		}*/
+		
+/*
+		for(Cadastro varredura:c1) 
+		{	
+			varredura.c1;
+			quant++;
+		}
+		quant--;
+*/
+		
+	}
+}
+
